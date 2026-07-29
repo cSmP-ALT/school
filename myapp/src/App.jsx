42e1c5265e698cea26d4a1a1538c26698b0c897b
CSMP <moraleschristophersantiago@gmail.com>
2026-07-29 12:29:49 -0600

    Fix: Build exitoso - Vite + React configurado correctamente

---
+++ myapp/src/App.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Aplicación</h1>
+      <p>¡Funcionando con Vite + React!</p>
+    </div>
+  )
+}
+
+export default App
